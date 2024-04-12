767a489bf50b07a228780e88f7d4aff813be84b7
XFernando43 <U20171E997@upc.edu.pe>
2024-04-12 18:32:57 -0500

    add: comments design

---
+++ src/components/Commons/CommentsComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container bg-red-400 shadow-2xl p-4 rounded-lg">
+    <h2 class="font-semibold text-2xl Slabe text-left">Comments:</h2>
+    <!-- <textarea name="textarea" rows="10" cols="50">Write something here</textarea>
+    -->
+
+    <div class="flex flex-col p-5 bg-slate-600">
+
+        <div class="flex flex-col bg-slate-400 p-4" v-for="(comment, index) in comments" :key="index">
+
+            <div class="flex flex-col bg-slate-300 p-4">
+                <h3 class=" rounded-xl text-lg font-semibold">{{ comment.owner }}</h3>
+                <p class=" Slabe font-mono">{{ comment.text }}</p>
+            </div>
+
+        </div>
+
+    </div>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      comments: [
+        {
+          owner: "Juan Pérez",
+          text: "¡Este artículo es realmente útil! Gracias por compartir esta información.",
+        },
+        {
+          owner: "María García",
+          text: "No estoy seguro de estar de acuerdo con todo lo que se dice aquí. Creo que hay otros puntos de vista a considerar.",
+        },
+        {
+          owner: "Carlos Rodríguez",
+          text: "Interesante perspectiva. Me gustaría aprender más sobre este tema.",
+        },
+        {
+          owner: "Ana Martínez",
+          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1400px;
+  height: 600px;
+}
+</style>
--- src/components/Pages/SismologicalPage.vue
+++ src/components/Pages/SismologicalPage.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="flex flex-col justify-center items-center p-10">
+    <div class="flex flex-col justify-center items-center p-10 gap-12">
 
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3904.79692287466!2d-77.08374282585076!3d-11.849484038257078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105d6b8edc07263%3A0xc8884fcd8baff4a0!2sAv.%20las%20Vi%C3%B1as%20760%2C%20Puente%20Piedra%2015121!5e0!3m2!1ses-419!2spe!4v1712953937924!5m2!1ses-419!2spe"
-            class="maps shadow-2xl" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3904.79692287466!2d-77.08374282585076!3d-11.849484038257078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105d6b8edc07263%3A0xc8884fcd8baff4a0!2sAv.%20las%20Vi%C3%B1as%20760%2C%20Puente%20Piedra%2015121!5e0!3m2!1ses-419!2spe!4v1712953937924!5m2!1ses-419!2spe"
+        class="maps shadow-2xl" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        <CommentsComponent />
 
     </div>
 
 </template>
 
 <script lang="ts">
+    import CommentsComponent from '../Commons/CommentsComponent.vue';
 
     export default{
         data(){
@@ -17,6 +19,9 @@
                 reports:[],
                 comments:[],
             }   
+        },
+        components:{
+            CommentsComponent
         }
     }
 
