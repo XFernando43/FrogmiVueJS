7407371b74bd97f5956cb0ad6be5eb3152aaaac6
XFernando43 <U20171E997@upc.edu.pe>
2024-04-12 15:17:44 -0500

    add: sismological pagge

---
+++ src/components/SismologicalPage.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script lang="ts">
+
+    export default{
+        data(){
+            return{
+                
+            }
+        }
+    }
+
+</script>
+
+<style scoped> 
+    
+</style>
