c5a5978d17b2e665ad266d76beb7fccba52d3dde
XFernando43 <U20171E997@upc.edu.pe>
2024-04-12 23:59:47 -0500

    add: reports page

---
+++ src/pages/ReportsPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-col gap-10">
+
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+            
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
