ed84a89bc9d2c5127f6aa3756c78340b1de4f4a3
XFernando43 <U20171E997@upc.edu.pe>
2024-04-15 13:02:55 -0500

    feature: google maps api refactored

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia';
+import {googleApi} from './googleMaps/googleApi.js';
+
 import './style.css'
 import App from './App.vue'
 import router from './router';
 
 const pinia = createPinia()
-
 const app = createApp(App);
+
+googleApi();
+
 app.use(router);
 app.use(pinia);
 app.mount('#app');
+
