d169d5dc2ca7e38c0f2d146503235e2338328c08
XFernando43 <U20171E997@upc.edu.pe>
2024-04-14 19:00:01 -0500

    add: comments fetching

--- src/components/Comments/Comments.component.vue
+++ src/components/Comments/Comments.component.vue
@@ -4,131 +4,56 @@
     <!-- <textarea name="textarea" rows="10" cols="50">Write something here</textarea>
     -->
 
-    <ComentTextArea/>
+    <ComentTextArea />
 
-    <div class="flex flex-col p-4" v-for="(comment, index) in comments" :key="index">
-        <div class="flex flex-col bg-slate-300 p-4 rounded-md">
-            <h3 class=" rounded-xl text-lg font-semibold">{{ comment.owner }}</h3>
-            <p class=" Slabe font-mono">{{ comment.text }}</p>
-        </div>
+    <div
+      class="flex flex-col p-4"
+      v-for="(comment, index) in comments"
+      :key="index"
+    >
+      <div class="flex flex-col bg-slate-300 p-4 rounded-md">
+        <h3 class="rounded-xl text-lg font-semibold">{{ comment.created_at }}</h3>
+        <p class="Slabe font-mono">{{ comment.body }}</p>
+      </div>
     </div>
-
 
 
   </div>
 </template>
 
 <script lang="ts">
-
-  import ComentTextArea from './ComentTextArea.vue';
+import ComentTextArea from "./ComentTextArea.vue";
+import { useCommentStore } from "../../stores/comment";
+import { mapActions, mapState } from "pinia";
 
 export default {
-
   data() {
-    return {
-      comments: [
-        {
-          owner: "Juan Pérez",
-          text: "¡Este artículo es realmente útil! Gracias por compartir esta información.",
-        },
-        {
-          owner: "María García",
-          text: "No estoy seguro de estar de acuerdo con todo lo que se dice aquí. Creo que hay otros puntos de vista a considerar.",
-        },
-        {
-          owner: "Carlos Rodríguez",
-          text: "Interesante perspectiva. Me gustaría aprender más sobre este tema.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-        {
-          owner: "Ana Martínez",
-          text: "¡Increíble trabajo! Me encantaría ver más contenido como este en el futuro.",
-        },
-      ],
-    };
+    return {};
   },
-  components:{
-    ComentTextArea
-  }
+  components: {
+    ComentTextArea,
+  },
+  computed: {
+    ...mapState(useCommentStore, ["comments"]),
+  },
+  methods: {
+    ...mapActions(useCommentStore, ["fetchComment"]),
+  },
+  props: {
+    reportId: {
+      type: Number,
+      required: true, 
+    },
+  },
+  mounted() {
+    this.fetchComment(this.reportId);
+  },
 };
 </script>
 
 <style scoped>
 .container {
   max-width: 1400px;
-  /* height: 100%; */
   height: 550px;
 }
-
 </style>
--- src/views/FeatureInfo.vue
+++ src/views/FeatureInfo.vue
@@ -1,32 +1,29 @@
 <template>
   <div class="flex flex-col justify-center items-center p-10 gap-12">
-    
     <h1>Report for Feature ID: {{ $route.params.id }}</h1>
 
     <div id="map" class="maps shadow-2xl"></div>
-    <CommentsComponent />
+    <CommentsComponent :reportId="featureId" />
 
   </div>
 </template>
 
 <script lang="ts">
-  import CommentsComponent from "../components/Comments/Comments.component.vue";
-  import { mapActions, mapState } from 'pinia';
-  import { useFeatureStore } from "../stores/features";
+import CommentsComponent from "../components/Comments/Comments.component.vue";
+import { mapActions, mapState } from 'pinia';
+import { useFeatureStore } from "../stores/features";
 
 export default {
   data() {
     return {
       map: null,
+      featureId: 0, // Inicializa featureId como 0
     };
   },
   mounted() {
     this.initMap();
-    const featureId = this.$route.params.id;
-    this.getFeatureByID(Number(featureId));
-    
-    
-    
+    this.featureId = Number(this.$route.params.id);
+    this.getFeatureByID(this.featureId); // Asegúrate de usar this.featureId aquí
     
   },
   methods: {
